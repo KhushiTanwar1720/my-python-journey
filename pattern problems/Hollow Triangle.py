d3b52b53739e01dde9b5ae482f06c5db836f1953
KhushiTanwar1720 <kr3218072@gmail.com>
2026-07-11 15:53:36 +0530

    Hollow Triangle

---
+++ pattern problems/Hollow Triangle.py
@@ -0,0 +1,8 @@
+i = 0
+for i in range(5):
+    for j in range(i+1):
+        if j == 0 or j == i or i == 4:
+            print("*",end = " ")
+        else:
+            print(" ",end = " ")
+    print()
