4ca9cff3726532e9a93cfb38dfb9a1f942b8f302
KhushiTanwar1720 <kr3218072@gmail.com>
2026-07-11 19:50:29 +0530

    Butterfly Pattern

---
+++ pattern problems/Butterfly Pattern 🦋.py
@@ -0,0 +1,17 @@
+i = 0
+for i in range(5):
+    for j in range(i+1):
+        print("*",end="")
+    for j in range(8-(i*2)):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end="")
+    print()
+for i in range(4):
+    for j in range(4-i):
+        print("*",end="")
+    for j in range((i*2)+2):
+        print(" ",end="")
+    for j in range(4-i):
+        print("*",end="")
+    print()
